8b426526cb3212ed01bb329d3032c7c6b0558395
aspirant0212-code <aspirant0212@gmail.com>
2026-02-22 19:01:02 +0530

    footer change

--- src/components/sections/footer.jsx
+++ src/components/sections/footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
                         <div className="col-lg-6">
                             <div className="copyright-text extra-copyright">
                                 <p>
-                                    Crafted with ❤️
+                                    Crafted with 💌
                                 </p>
                             </div>
                         </div>
